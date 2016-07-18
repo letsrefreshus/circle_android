ef82cc1a458b8c2fe62f3a9f0db2035e3e31e65c
jinwoo7 <jinwoo7@vt.edu>
2016-07-17 22:19:25 -0700

    Updates on framework

--- circle_refreshus/app/src/main/java/com/refreshus/jinwoo/circle_refreshus/ListActivity.java
+++ circle_refreshus/app/src/main/java/com/refreshus/jinwoo/circle_refreshus/ListActivity.java
@@ -1,16 +1,28 @@
 package com.refreshus.jinwoo.circle_refreshus;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -18,7 +30,30 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        Button NwkBtn = (Button) findViewById(R.id.NetworkBtn);
+        NwkBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                ConnectivityManager connMgr = (ConnectivityManager)
+                        getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if(networkInfo != null && networkInfo.isConnected()){
+                    // fetch data
+                    Toast.makeText(ListActivity.this,"Connected to the web!", Toast.LENGTH_LONG).show();
+                } else{
+                    // display error
+                    Toast.makeText(ListActivity.this,"Device not connected to network", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+/*
+    public void myClickHandler(View view){
+
+
+    }*/
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
